8d853a96120df868a3def73a6786508f6c378b12
Pierre Canthelou <pierre.canthelou@passculture.app>
2023-06-28 00:04:58 +0200

    Refactor core store

--- src/hooks/useTokenReaderStore.js
+++ src/hooks/useTokenReaderStore.js
@@ -1,21 +1,58 @@
 import { create } from "zustand"
-import { defaultBookingDetails, getBookingDetails } from "../api/getBookingDetails"
+import getBookingDetails, {
+    defaultBookingDetails,
+} from "../api/getBookingDetails"
 
 const initialStore = {
+    resetKey: 0,
     tokenRead: "",
     tokenReadErrorCode: 0,
-    bookingDetails: defaultBookingDetails
+    bookingDetails: defaultBookingDetails,
 }
 
-const useTokenReaderStore = create((set) => (initialStore))
-
-export const resetTokenRead = () =>
-useTokenReaderStore.setState(() => (initialStore))
-
-export const readToken = (newToken) => {
-    // if (newToken.length === 6) {
-    //     getBookingDetails(newToken)
-    // }
-}
+const useTokenReaderStore = create((set, get) => ({
+    ...initialStore,
+    resetTokenRead: () =>
+        set((state) => ({
+            ...initialStore,
+            resetKey: state.tokenRead + Date.now(),
+        })),
+    isTokenExist: () => get().tokenRead.length > 0,
+    isTokenValid: () => {
+        const { tokenRead, bookingDetails, tokenReadErrorCode } = get()
+        return (
+            tokenRead.length > 0 &&
+            tokenReadErrorCode === "200" &&
+            bookingDetails !== defaultBookingDetails
+        )
+    },
+    isTokenAlreadyScanned: () => {
+        const { tokenRead, bookingDetails, tokenReadErrorCode } = get()
+        return (
+            tokenRead.length > 0 &&
+            tokenReadErrorCode === "410" &&
+            bookingDetails !== defaultBookingDetails
+        )
+    },
+    readToken: (newToken) => {
+        let apiResult = undefined
+        if (newToken.length === 6) {
+            apiResult = getBookingDetails(newToken)
+            set({
+                tokenRead: newToken,
+                ...apiResult,
+            })
+        }
+    },
+    toValidate: () => {
+        get().resetTokenRead()
+    },
+    toInvalidate: () => {
+        get().resetTokenRead()
+    },
+    toRetry: () => {
+        get().readToken(get().tokenRead)
+    },
+}))
 
 export default useTokenReaderStore
