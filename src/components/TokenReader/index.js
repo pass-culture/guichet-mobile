d72f3784e06bc4d4e05ad268055de738b3097a5c
Pierre Canthelou <pierre.canthelou@passculture.app>
2023-06-28 00:05:15 +0200

    The main workflow

--- src/components/TokenReader/index.js
+++ src/components/TokenReader/index.js
@@ -1,37 +1,59 @@
 import { useState, useEffect } from "react"
 import useTokenReaderStore from "../../hooks/useTokenReaderStore"
 import { GmButton, GmText } from "../../ui-kit"
-import { BookingDetails } from "../BookingDetails"
+import BookingDetails from "../BookingDetails"
 import { shallow } from "zustand/shallow"
 
 export default TokenReader = () => {
     // Object pick, re-renders the component when either state.nuts or state.honey change
-    const { tokenRead, bookingDetails } = useTokenReaderStore(
-        (state) => ({
-            tokenRead: state.tokenRead,
-            bookingDetails: state.bookingDetails,
-        }),
-        shallow,
-    )
+    const {
+        tokenRead,
+        bookingDetails,
+        isTokenExist,
+        isTokenValid,
+        isTokenAlreadyScanned,
+        toValidate,
+        toInvalidate,
+        toRetry,
+    } = useTokenReaderStore()
 
-    if (!tokenRead) {
-        return <GmText>Veuillez scanner une contremarque</GmText>
+    if (!isTokenExist()) {
+        return (
+            <>
+                <GmText>Veuillez scanner une contremarque</GmText>
+            </>
+        )
     }
 
-    return bookingDetails ? (
+    if (isTokenAlreadyScanned()) {
+        return (
+            <>
+                <GmText>Déjà validé</GmText>
+                <BookingDetails
+                    userName={bookingDetails.userName}
+                    offerName={bookingDetails.offerName}
+                    date={bookingDetails.datetime}
+                    price={bookingDetails.price}
+                />
+                <GmButton title="Invalider" onPress={toInvalidate} />
+            </>
+        )
+    }
+
+    return isTokenValid() ? (
         <>
             <BookingDetails
-                userName={"userName"}
-                offerName={"offerName"}
-                date={"datetime"}
-                price={"price"}
+                userName={bookingDetails.userName}
+                offerName={bookingDetails.offerName}
+                date={bookingDetails.datetime}
+                price={bookingDetails.price}
             />
-            <GmButton title="Valider" onPress={() => {}} />
+            <GmButton title="Valider" onPress={toValidate} />
         </>
     ) : (
         <>
             <GmText>La contremarque n'existe pas</GmText>
-            <GmButton title="Reessayer" onPress={() => {}} />
+            <GmButton title="Reessayer" onPress={toRetry} />
         </>
     )
 }
