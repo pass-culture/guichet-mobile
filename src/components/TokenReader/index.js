a5e470f1efac3a2a351c6c534a07531144cb097a
Pierre Canthelou <pierre.canthelou@passculture.app>
2023-06-27 22:40:59 +0200

    Show correct inital state for nothing is read

--- src/components/TokenReader/index.js
+++ src/components/TokenReader/index.js
@@ -2,12 +2,21 @@ import { useState, useEffect } from "react"
 import useTokenReaderStore from "../../hooks/useTokenReaderStore"
 import { GmButton, GmText } from "../../ui-kit"
 import { BookingDetails } from "../BookingDetails"
+import { shallow } from "zustand/shallow"
 
 export default TokenReader = () => {
-    const bookingDetails = false
-    // if (showDefault) {
-    //     return <GmText>Veuillez scanner une contremarque</GmText>
-    // }
+    // Object pick, re-renders the component when either state.nuts or state.honey change
+    const { tokenRead, bookingDetails } = useTokenReaderStore(
+        (state) => ({
+            tokenRead: state.tokenRead,
+            bookingDetails: state.bookingDetails,
+        }),
+        shallow,
+    )
+
+    if (!tokenRead) {
+        return <GmText>Veuillez scanner une contremarque</GmText>
+    }
 
     return bookingDetails ? (
         <>
