d97c6e0547b96054b38cf7c67c5e3c1fd04e6aef
Pierre Canthelou <pierre.canthelou@passculture.app>
2023-06-27 22:34:17 +0200

    Restore main index due to error of stash

--- app/index.js
+++ app/index.js
@@ -1,6 +1,18 @@
-import { Text } from "react-native"
-import { Link } from "expo-router"
+import { Text, View, StyleSheet } from "react-native"
+import { QrCode, TokenInput, TokenReader } from "../src/components"
 
 export default function Home() {
-    return <Link href="/desk">Desk</Link>
+    return (
+        <View style={styles.wrapper}>
+            <QrCode />
+            <View>
+                <TokenInput />
+            </View>
+            <TokenReader />
+        </View>
+    )
 }
+
+const styles = StyleSheet.create({
+    wrapper: {},
+})
