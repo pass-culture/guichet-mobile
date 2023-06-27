71df4f2b7cef9282c24bd4bbe93c783ae7dc721d
Pierre Canthelou <pierre.canthelou@passculture.app>
2023-06-27 22:34:41 +0200

    Remove QRCodeCheck no more necessary

--- src/components/index.js
+++ src/components/index.js
@@ -1,5 +1,4 @@
 export { default as BookingDetails } from "./BookingDetails/index"
 export { default as QrCode } from "./QrCode/index.js"
-export { default as QrCodeCheck } from "./QrCodeCheck/index.js"
 export { default as TokenInput } from "./TokenInput/index"
 export { default as TokenReader } from "./TokenReader/index"
