4f6aed7f99fecdb277d5f472af6ecdef3bb091f4
Pierre Canthelou <pierre.canthelou@passculture.app>
2023-06-28 11:17:25 +0200

    Fix position of blocks

--- app/index.js
+++ app/index.js
@@ -1,18 +1,54 @@
-import { Text, View, StyleSheet } from "react-native"
+import { StyleSheet } from "react-native"
+import {
+    GmText,
+    GmGmView,
+    drawerBorderRadius,
+    drawerHeight,
+} from "../src/ui-kit/index"
 import { QrCode, TokenInput, TokenReader } from "../src/components"
 
 export default function Home() {
     return (
-        <View style={styles.wrapper}>
-            <QrCode />
-            <View>
-                <TokenInput />
-            </View>
-            <TokenReader />
-        </View>
+        <>
+            <GmView style={styles.qrCodeWrapper}>
+                <QrCode style={styles.qrCodeWrapper} />
+            </GmView>
+            <GmView style={styles.wrapper}>
+                <GmView style={styles.drawer}>
+                    <GmText>Contremarque</GmText>
+                    <TokenInput />
+                    <TokenReader />
+                </GmView>
+            </GmView>
+        </>
     )
 }
 
 const styles = StyleSheet.create({
-    wrapper: {},
+    wrapper: {
+        position: "absolute",
+        top: 0,
+        left: 0,
+        width: "100%",
+        height: "100%",
+        flex: 1,
+        zIndex: 1,
+    },
+    drawer: {
+        position: "absolute",
+        bottom: 0,
+        left: 0,
+        width: "100%",
+        height: drawerHeight,
+        backgroundColor: "white",
+        borderTopLeftRadius: drawerBorderRadius,
+        borderTopRightRadius: drawerBorderRadius,
+        paddingTop: drawerBorderRadius,
+        paddingHorizontal: drawerBorderRadius,
+    },
+    qrCodeWrapper: {
+        width: "100%",
+        height: "100%",
+        backgroundColor: "#5A5A5A",
+    },
 })
