6446cd55026dbf09fe5a2f6d887e096c483dfe29
antoinemrn <antoine.marinier@passculture.app>
2023-06-28 10:03:22 +0200

    Add style for gm button with variant

--- src/ui-kit/GmButton/index.js
+++ src/ui-kit/GmButton/index.js
@@ -1,15 +1,49 @@
-import { Button, StyleSheet } from "react-native"
+import { Text, Pressable, StyleSheet } from "react-native"
 
-export default GmButton = ({ title, onPress, customStyle }) => {
+export default GmButton = ({ title, onPress, customStyle, variant='PRIMARY' }) => {
+    const variantStyle = variant === 'PRIMARY' ? styles.gmButtonPrimary : styles.gmButtonSecondary
+    const variantTextStyle = variant === 'PRIMARY' ? styles.gmButtonTextPrimary : styles.gmButtonTextSecondary
     return (
-        <Button
-            title={title}
-            onPress={onPress}
-            style={{ ...styles.gmButton, ...customStyle }}
-        />
+        <Pressable onPress={onPress} style={{ ...styles.gmButton, ...customStyle, ...variantStyle }}>
+            <Text style={{...styles.gmButtonText, ...variantTextStyle}}>{title}</Text>
+        </Pressable>
     )
 }
 
 const styles = StyleSheet.create({
-    gmButton: {},
+    gmButton: {
+        display: "flex",
+        flexDirection: "column",
+        alignItems: "center",
+        justifyContent: "center",
+        gap: "10px",
+        backgroundColor:"#EB0055",
+        paddingVertical: "10px",
+        paddingHorizontal: "24px",
+        borderRadius: "30px",
+        height: "40px",
+        width: "238px"
+    },
+    gmButtonPrimary: {
+        backgroundColor:"#EB0055",
+    },
+    gmButtonSecondary: {
+        backgroundColor:"#FFFFFF",
+        border: "2px solid #EB0055"
+    },
+    gmButtonText: {
+        size: "15px",
+        height: "20px",
+        font: "Montserrat",
+        fontWeight: "700",
+        display: "flex",
+        alignItems: "center",
+        justifyContent: "center",
+    },
+    gmButtonTextPrimary: {
+        color: "#FFFFFF",
+    },
+    gmButtonTextSecondary: {
+        color: "#EB0055",
+    },
 })
