e0c1d6f5b993fbf7113ab1b2266fd201433f0e52
Pierre Canthelou <pierre.canthelou@passculture.app>
2023-06-28 10:00:29 +0200

    Usage of the store and add permissions

--- src/components/QrCode/index.js
+++ src/components/QrCode/index.js
@@ -1,11 +1,44 @@
-import { Text, View, StyleSheet } from "react-native"
+import { useState, useEffect } from "react"
+import { Text, View, StyleSheet, Button } from "react-native"
+import { BarCodeScanner } from "expo-barcode-scanner"
+import useTokenReaderStore from "../../hooks/useTokenReaderStore"
 
-export default QrCode = () => (
-    <View style={styles.qrCodeWrapper}>
-        <Text>This is the placeholder of the QR Code scanner</Text>
-    </View>
-)
+export default QrCode = () => {
+    const [hasPermission, setHasPermission] = useState(null)
+    const [scanned, setScanned] = useState(false)
+    const { readToken } = useTokenReaderStore()
+
+    useEffect(() => {
+        const getBarCodeScannerPermissions = async () => {
+            const { status } = await BarCodeScanner.requestPermissionsAsync()
+            setHasPermission(status === "granted")
+        }
+
+        getBarCodeScannerPermissions()
+    }, [])
+
+    const handleBarCodeScanned = ({ type, data }) => {
+        readToken(data)
+        setScanned(false)
+    }
+
+    if (hasPermission === null) {
+        return <Text>Requesting for camera permission</Text>
+    }
+    if (hasPermission === false) {
+        return <Text>No access to camera</Text>
+    }
+
+    return (
+        <View style={styles.container}>
+            <BarCodeScanner
+                onBarCodeScanned={scanned ? undefined : handleBarCodeScanned}
+                style={StyleSheet.absoluteFillObject}
+            />
+        </View>
+    )
+}
 
 const styles = StyleSheet.create({
-    qrCodeWrapper: { width: "100%", height: 200, backgroundColor: "grey" },
+    container: { width: "100%", height: 200, backgroundColor: "grey" },
 })
